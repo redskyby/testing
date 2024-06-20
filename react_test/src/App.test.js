58b20c7367c37079642f8d3ea8b0f6c16dbd2f7c
Docenko Pasha <pashadocenko@gmail.com>
2024-06-20 23:05:29 +0300

    react was added.

--- react_test/src/App.test.js
+++ react_test/src/App.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { act } from "react";
 
-test("renders hello world text", () => {
-    render(<App />);
-    const helloWorld = screen.getByText(/hello world/i);
-    expect(helloWorld).toBeInTheDocument();
-    screen.debug();
-});
+describe("test app" , ()=>{
+    test("renders hello world text", () => {
+        render(<App />);
+        const helloWorld = screen.getByText(/hello world/i);
+        expect(helloWorld).toBeInTheDocument();
+        screen.debug();
+    });
+})
+
