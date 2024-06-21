4ecaa7dc5d6174551ea38bf6b0d71672ef04ec1f
Docenko Pasha <pashadocenko@gmail.com>
2024-06-21 23:54:02 +0300

    findByText was added.

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -1,6 +1,17 @@
+import {useEffect, useState} from "react";
+
 function App() {
+    const [data, setData] = useState(null)
+    useEffect(() => {
+            setTimeout(() => {
+                setData({})
+            }, 100)
+        },
+        [])
+
     return (
         <div>
+            {data && <div>data</div>}
             <h1>hello world</h1>
         </div>
     );
--- react_test/src/App.test.js
+++ react_test/src/App.test.js
@@ -1,10 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import App from "./App";
 
 describe("test app", () => {
-    test("renders hello world text", () => {
-        render(<App />);
-        const helloWorld = screen.queryByText(/hello2/i);
-        expect(helloWorld).toBeNull();
+    test("renders hello world text", async () => {
+        render(<App/>);
+        // const helloWorld = screen.queryByText(/hello2/i);
+        // expect(helloWorld).toBeNull();
+        const helloWorld = await screen.findByText(/data/i);
+        expect(helloWorld).toBeInTheDocument();
     });
 });
