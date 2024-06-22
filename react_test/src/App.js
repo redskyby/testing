01e0a7e8aabbc504113828edf7c1f7f17e3bf9a6
Docenko Pasha <pashadocenko@gmail.com>
2024-06-23 01:11:22 +0300

    input check

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [data, setData] = useState(null);
     const [toggle, setToggle] = useState(false);
+    const [value, setValue] = useState("");
 
     const onClick = () => setToggle((prev) => !prev);
 
@@ -14,12 +15,14 @@ function App() {
 
     return (
         <div>
+            <h1 data-testid={"value-elem"}>{value}</h1>
             {toggle === true && <div data-testid="toggle-elem">toggle</div>}
             {data && <div>data</div>}
             <h1>hello world</h1>
             <button onClick={onClick} data-testid="toggle-btn">
                 click me
             </button>
+            <input onChange={e => setValue(e.target.value)} type="text" placeholder={"input value...."}/>
         </div>
     );
 }
--- react_test/src/App.test.js
+++ react_test/src/App.test.js
@@ -1,26 +1,36 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {render, screen, fireEvent} from "@testing-library/react";
 import App from "./App";
 
 describe("test app", () => {
     test("renders hello world text", async () => {
-        render(<App />);
+        render(<App/>);
         const helloWorld = screen.queryByText(/hello2/i);
         expect(helloWorld).toBeNull();
     });
 
     test("check async", async () => {
-        render(<App />);
+        render(<App/>);
         const helloWorld = await screen.findByText(/data/i);
         expect(helloWorld).toBeInTheDocument();
     });
 
     test("check click", () => {
-        render(<App />);
+        render(<App/>);
         const btn = screen.getByTestId("toggle-btn");
         expect(screen.queryByTestId("toggle-elem")).toBeNull();
         fireEvent.click(btn);
         expect(screen.queryByTestId("toggle-elem")).toBeInTheDocument();
         fireEvent.click(btn);
         expect(screen.queryByTestId("toggle-elem")).toBeNull();
+    });
+
+    test("input check", () => {
+        render(<App/>);
+        const input = screen.getByPlaceholderText(/input value/i);
+        expect(screen.queryByTestId("value-elem")).toContainHTML("");
+        fireEvent.input(input , {
+            target : {value : "1231233"}
+        });
+        expect(screen.queryByTestId("value-elem")).toContainHTML("1231233");
     });
 });
