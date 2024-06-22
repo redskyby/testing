7092e2a1ac4a82a0575a53bcdcfd43dab8f886c0
Docenko Pasha <pashadocenko@gmail.com>
2024-06-22 23:41:50 +0300

    check async

--- react_test/src/App.test.js
+++ react_test/src/App.test.js
@@ -4,8 +4,12 @@ import App from "./App";
 describe("test app", () => {
     test("renders hello world text", async () => {
         render(<App />);
-        // const helloWorld = screen.queryByText(/hello2/i);
-        // expect(helloWorld).toBeNull();
+        const helloWorld = screen.queryByText(/hello2/i);
+        expect(helloWorld).toBeNull();
+    });
+
+    test("check async", async () => {
+        render(<App />);
         const helloWorld = await screen.findByText(/data/i);
         expect(helloWorld).toBeInTheDocument();
     });
