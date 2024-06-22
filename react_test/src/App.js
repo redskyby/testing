90e005cdf900f919e80ec9cc4f329daf2690725b
Docenko Pasha <pashadocenko@gmail.com>
2024-06-23 00:06:03 +0300

    check click

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 
 function App() {
     const [data, setData] = useState(null);
+    const [toggle , setToggle] = useState(false)
+
+    const onClick = () => setToggle(prev => !prev)
+
     useEffect(() => {
         setTimeout(() => {
             setData({});
@@ -10,8 +14,10 @@ function App() {
 
     return (
         <div>
+            {toggle === true && <div data-testid="toggle-elem">toggle</div>}
             {data && <div>data</div>}
             <h1>hello world</h1>
+            <button onClick={onClick} data-testid="toggle-btn">click me</button>
         </div>
     );
 }
--- react_test/src/App.test.js
+++ react_test/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 describe("test app", () => {
@@ -12,5 +12,15 @@ describe("test app", () => {
         render(<App />);
         const helloWorld = await screen.findByText(/data/i);
         expect(helloWorld).toBeInTheDocument();
+    });
+
+    test("check click",  () => {
+        render(<App />);
+        const btn = screen.getByTestId("toggle-btn")
+        expect(screen.queryByTestId("toggle-elem")).toBeNull()
+        fireEvent.click(btn)
+        expect(screen.queryByTestId("toggle-elem")).toBeInTheDocument();
+        fireEvent.click(btn)
+        expect(screen.queryByTestId("toggle-elem")).toBeNull()
     });
 });
