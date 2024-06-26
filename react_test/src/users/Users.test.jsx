66b3ec4e13e83c7b604a49d550d0e45b789ed0e2
Docenko Pasha <pashadocenko@gmail.com>
2024-06-26 13:32:13 +0300

    tests for Users.jsx were added.

--- react_test/src/users/Users.test.jsx
+++ react_test/src/users/Users.test.jsx
@@ -1,8 +1,33 @@
 import {render} from "@testing-library/react";
 import Users from "./Users";
+import axios from "axios";
 
 jest.mock("axios")
 describe("Users test", () => {
+    let response;
+    beforeEach(() => {
+        response = {
+            data: [
+                {
+                    "id": 1,
+                    "name": "Leanne Graham",
+                },
+                {
+                    "id": 2,
+                    "name": "Ervin Howell",
+                },
+                {
+                    "id": 3,
+                    "name": "Clementine Bauch",
+                },
+            ]
+        }
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    })
+
     test("test render", () => {
         render(<Users/>);
 
